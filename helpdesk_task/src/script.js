c952a20de49970a5dc3ea3237c8b9ba09af37ac8
Ashish Kaloge <110806467+ashishkaloge@users.noreply.github.com>
2023-07-07 11:07:45 +0530

    Add files via upload

--- helpdesk_task/src/script.js
+++ helpdesk_task/src/script.js
@@ -1,15 +1,55 @@
 const main = document.querySelector(".main-window");
 const helpBtn = document.getElementById("helpButton");
+let iframeVisible = false; 
+let iframeContainer = null;
 
 helpBtn.addEventListener('click', function() {
-    const newDiv = document.createElement('div');
-    newDiv.classList.add("iframe-container");
-    
-    const iframe = document.createElement('iframe');
-    iframe.src = "https://helpdesk.uffizio.com/knowledge";
-    iframe.style.width= "600px"
-    
-    newDiv.appendChild(iframe);
-    main.appendChild(newDiv);
+    if (iframeVisible) {
+        hideIframe();
+    } else {
+        showIframe();
+    }
 });
+
+function showIframe() {
+    if (!iframeContainer) {
+        createIframe();
+    }
+    iframeContainer.style.display = "block";
+    iframeVisible = true;
+}
+
+function hideIframe() {
+    if (iframeContainer) {
+        iframeContainer.style.display = "none";
+    }
+    iframeVisible = false;
+}
+
+function createIframe() {
+    iframeContainer = document.createElement('div');
+    iframeContainer.classList.add("iframe-container");
+
+    const iframe = document.createElement('iframe');
+    iframe.src = "https://helpdesk.uffizio.com/knowledge/trakzee-platform";
+
+    const closeButton = document.createElement('button');
+    closeButton.innerText = "Close";
+    closeButton.classList.add("close-btn")
+    
+    closeButton.addEventListener('click', hideIframe);
+
+    const contentContainer = document.createElement('div');
+    contentContainer.appendChild(closeButton);
+    contentContainer.appendChild(iframe);
+    contentContainer.classList.add("container-iframe"); 
+
+
+    iframeContainer.appendChild(contentContainer);
+    main.appendChild(iframeContainer);
+}
+
+
+
+
 
