5ada566d1eddeb795eb57923abb34ca3eac1c131
Ashish Kaloge <110806467+ashishkaloge@users.noreply.github.com>
2023-07-17 14:08:51 +0530

    Add files via upload

--- form validation/script.js
+++ form validation/script.js
@@ -6,45 +6,48 @@ const intermittentlyOccursCheckbox = document.getElementById("checkIntermittentl
 const detailsInput = document.getElementById("inputDetails");
 const radioValidationMsg = document.getElementById("radioValidationMsg");
 
-nameInput.addEventListener('input', validateName);
-emailInput.addEventListener('input', validateEmail);
-alwaysOccursCheckbox.addEventListener('input', validateReproducibility);
-intermittentlyOccursCheckbox.addEventListener('input', validateReproducibility);
-detailsInput.addEventListener('input', validateDetails);
 
-submit.addEventListener('click', submitForm);
+String.prototype.isEmpty = function() {
+    return this.trim() === "";
+};
 
-function validateName() {
+String.prototype.hasMinimumLength = function(length) {
+    return this.length >= length;
+};
+
+String.prototype.hasValidEmailFormat = function() {
+    return /^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,})+$/.test(this);
+};
+
+const validateName = () => {
     const nameValidationMessage = document.getElementById("nameValidationMessage");
     nameValidationMessage.textContent = "";
 
     let name = nameInput.value.trim();
 
-    if (name === "") {
-        nameValidationMessage.textContent = "Name cannot be empty";
-    } else if (name.length < 3) {
-        nameValidationMessage.textContent = "Name should contain at least 3 characters";
+    if (name.isEmpty()) {
+        nameValidationMessage.textContent = "Please enter your name";
+    } else if (!name.hasMinimumLength(3)) {
+        nameValidationMessage.textContent = "Name should contain at least 3 alphabetical characters";
     } else if (name.includes(' ')) {
-        nameValidationMessage.textContent = "Spaces not allowed in between name";
+        nameValidationMessage.textContent = "Spaces are not allowed in the name";
     }
-}
+};
 
-function validateEmail() {
+const validateEmail = () => {
     const emailValidationMessage = document.getElementById("emailValidationMessage");
     emailValidationMessage.textContent = "";
 
     let email = emailInput.value.trim();
 
-    if (email === "") {
-        emailValidationMessage.textContent = "Email cannot be empty";
-    } else if (!email.includes("@")) {
-        emailValidationMessage.textContent = "Email should contain '@'";
-    } else if (!email.endsWith(".com")) {
-        emailValidationMessage.textContent = "Email should end with '.com'";
+    if (email.isEmpty()) {
+        emailValidationMessage.textContent = "Please enter your email";
+    } else if (!email.hasValidEmailFormat()) {
+        emailValidationMessage.textContent = "Invalid email format";
     }
-}
+};
 
-function validateReproducibility() {
+const validateReproducibility = () => {
     const reproducibilityValidationMessage = document.getElementById("reproducibilityValidationMessage");
     reproducibilityValidationMessage.textContent = "";
 
@@ -52,24 +55,24 @@ function validateReproducibility() {
     let intermittentlyOccurs = intermittentlyOccursCheckbox.checked;
 
     if (!alwaysOccurs && !intermittentlyOccurs) {
-        reproducibilityValidationMessage.textContent = "At least one checkbox needed";
+        reproducibilityValidationMessage.textContent = "Please select at least one option for reproducibility";
     }
-}
+};
 
-function validateDetails() {
+const validateDetails = () => {
     const detailsValidationMessage = document.getElementById("detailsValidationMessage");
     detailsValidationMessage.textContent = "";
 
     let details = detailsInput.value.trim();
 
-    if (details === "") {
-        detailsValidationMessage.textContent = "Details cannot be empty";
-    } else if (details.length < 50) {
-        detailsValidationMessage.textContent = "Explain briefly, at least 50 characters are required";
+    if (details.isEmpty()) {
+        detailsValidationMessage.textContent = "Please provide details about the issue";
+    } else if (!details.hasMinimumLength(50)) {
+        detailsValidationMessage.textContent = "Please provide a detailed explanation (at least 50 characters)";
     }
-}
+};
 
-function submitForm(event) {
+const submitForm = (event) => {
     event.preventDefault();
 
     validateName();
@@ -111,7 +114,17 @@ function submitForm(event) {
     }
 
     radioValidationMsg.textContent = radioValidationMsgText;
-}
+};
+
+
+nameInput.addEventListener('input', validateName);
+emailInput.addEventListener('input', validateEmail);
+alwaysOccursCheckbox.addEventListener('input', validateReproducibility);
+intermittentlyOccursCheckbox.addEventListener('input', validateReproducibility);
+detailsInput.addEventListener('input', validateDetails);
+
+submit.addEventListener('click', submitForm);
+
 
 
 
